9360843e36e177368e78e80e18bd78cc5de06d9c
Noah <nes9968@rit.edu>
2018-11-15 17:47:00 -0500

    Updated c21 to Alberto's

--- C21_SpatialOptimizationPt1/MyOctant.cpp
+++ C21_SpatialOptimizationPt1/MyOctant.cpp
@@ -7,13 +7,73 @@ void MyOctant::Init(void)
 	m_nData = 0;
 	m_pMeshMngr = MeshManager::GetInstance();
 	m_pEntityMngr = MyEntityManager::GetInstance();
-	
+	//IsColliding();
 	for (uint i = 0; i < 8; i++)
 	{
 		m_pChild[i] = nullptr;
 	}
-	//IsColliding();
 }
+
+MyOctant::MyOctant()
+{
+	Init();
+	std::vector<MyEntity*> l_Entity_List = m_pEntityMngr->GetEntityList();
+	uint iEntityCount = l_Entity_List.size();
+	std::vector<vector3> v3MaxMin_list;
+	for (uint i = 0; i < iEntityCount; ++i)
+	{
+		MyRigidBody* pRG = l_Entity_List[i]->GetRigidBody();
+		vector3 v3Min = pRG->GetMinGlobal();
+		vector3 v3Max = pRG->GetMaxGlobal();
+		v3MaxMin_list.push_back(v3Min);
+		v3MaxMin_list.push_back(v3Max);
+	}
+
+	m_pRigidBody = new MyRigidBody(v3MaxMin_list);
+	m_pRigidBody->MakeCubic();
+	m_iID = m_nCount;
+	Subdivide();
+}
+
+MyOctant::MyOctant(vector3 a_v3Center, float a_fSize)
+{
+	Init();
+	std::vector<vector3> v3MaxMin_list;
+	v3MaxMin_list.push_back(a_v3Center - vector3(a_fSize));
+	v3MaxMin_list.push_back(a_v3Center + vector3(a_fSize));
+	m_pRigidBody = new MyRigidBody(v3MaxMin_list);
+	m_nCount++;
+	m_iID = m_nCount;
+}
+
+void MyOctant::Subdivide()
+{
+	if (m_nLevel > 3)
+		return;
+
+	vector3 v3Center = m_pRigidBody->GetCenterLocal();
+	vector3 v3HalfWidth = m_pRigidBody->GetHalfWidth();
+	float fSize = (v3HalfWidth.x) / 2.0f;
+	float fCenters = fSize;
+
+	m_pChild[0] = new MyOctant(v3Center + vector3( fCenters, fCenters, fCenters), fSize);
+	m_pChild[1] = new MyOctant(v3Center + vector3(-fCenters, fCenters, fCenters), fSize);
+	m_pChild[2] = new MyOctant(v3Center + vector3(-fCenters,-fCenters, fCenters), fSize);
+	m_pChild[3] = new MyOctant(v3Center + vector3( fCenters,-fCenters, fCenters), fSize);
+	
+	m_pChild[4] = new MyOctant(v3Center + vector3( fCenters, fCenters,-fCenters), fSize);
+	m_pChild[5] = new MyOctant(v3Center + vector3(-fCenters, fCenters,-fCenters), fSize);
+	m_pChild[6] = new MyOctant(v3Center + vector3(-fCenters,-fCenters,-fCenters), fSize);
+	m_pChild[7] = new MyOctant(v3Center + vector3( fCenters,-fCenters,-fCenters), fSize);
+	
+	for (uint i = 0; i < 8; i++)
+	{
+		m_pChild[i]->m_nLevel = m_nLevel + 1;
+		m_pChild[i]->m_pParent = this;
+		m_pChild[i]->Subdivide();
+	}
+}
+
 void MyOctant::Swap(MyOctant& other)
 {
 	std::swap(m_nData, other.m_nData);
@@ -45,62 +105,6 @@ void Simplex::MyOctant::IsColliding(void)
 			l_Entity_List[i]->AddDimension(m_iID);
 		}
 	}
-}
-void MyOctant::Subdivide()
-{
-	if (m_nLevel > 1)
-		return;
-	vector3 v3Center = m_pRigidBody->GetCenterGlobal();
-	vector3 v3HalfWidth = m_pRigidBody->GetHalfWidth();
-	float fSize = (v3HalfWidth.x)  /2.0f;
-	float fCenters = fSize;
-
-	// memory leaks, need to solve
-	m_pChild[0] = new MyOctant(v3Center + vector3( fCenters, fCenters, fCenters), fSize);
-	m_pChild[1] = new MyOctant(v3Center + vector3(-fCenters, fCenters, fCenters), fSize);
-	m_pChild[2] = new MyOctant(v3Center + vector3(-fCenters,-fCenters, fCenters), fSize);
-	m_pChild[3] = new MyOctant(v3Center + vector3( fCenters,-fCenters, fCenters), fSize);
-
-	m_pChild[4] = new MyOctant(v3Center + vector3(fCenters, fCenters, -fCenters), fSize);
-	m_pChild[5] = new MyOctant(v3Center + vector3(-fCenters, fCenters,-fCenters), fSize);
-	m_pChild[6] = new MyOctant(v3Center + vector3(-fCenters,-fCenters,-fCenters), fSize);
-	m_pChild[7] = new MyOctant(v3Center + vector3(fCenters, -fCenters,-fCenters), fSize);
-	for (uint i = 0; i < 8; i++)
-	{
-		m_pChild[i]->m_nLevel += 1;
-		m_pChild[i]->m_pParent = this;
-		m_pChild[i]->Subdivide();
-	}
-}
-
-MyOctant::MyOctant(vector3 a_v3Center, float a_fSize)
-{
-	Init();
-	std::vector<vector3> v3MaxMin_list;
-	v3MaxMin_list.push_back(a_v3Center - vector3(a_fSize));
-	v3MaxMin_list.push_back(a_v3Center + vector3(a_fSize));
-	m_pRigidBody = new MyRigidBody(v3MaxMin_list);
-	m_nCount++;
-	m_iID = m_nCount;
-}
-//The big 3
-MyOctant::MyOctant()
-{
-	Init();
-	std::vector<MyEntity*> l_Entity_List = m_pEntityMngr->GetEntityList();
-	uint iEntityCount = l_Entity_List.size();
-	std::vector<vector3> v3MaxMin_list;
-	for (uint i = 0; i < iEntityCount; ++i)
-	{
-		MyRigidBody* pRG = l_Entity_List[i]->GetRigidBody();
-		vector3 v3Min = pRG->GetMinGlobal();
-		vector3 v3Max = pRG->GetMaxGlobal();
-		v3MaxMin_list.push_back(v3Min);
-		v3MaxMin_list.push_back(v3Max);
-	}
-	m_pRigidBody = new MyRigidBody(v3MaxMin_list);
-	m_pRigidBody->MakeCubic();
-	Subdivide();
 }
 MyOctant::MyOctant(MyOctant const& other)
 {
